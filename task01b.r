1b203cee810bcced2bb5d0430166d30b10a444c3
hannanjackson <hnj0004@mix.wvu.edu>
2021-01-27 17:26:26 -0500

    <Hanna Jackson> Task 01 upload b

---
+++ task01b.r
@@ -0,0 +1,30 @@
+install.packages ('swirl')
+library ('swirl')
+swirl ()
+Hanna
+1
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+z <- c(555)
+c(z, 555, z)
+z * 2 +100
+my_sqrt <- sqrt(z-1)
+2
+my_sqrt
+my_div <- z / my_sqrt
+2
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 +1000
+my_div
+1
