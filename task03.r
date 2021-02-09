38bbb09c3ae1d8e9c511476c79664ef0f0d41c52
hannanjackson <77799882+hannanjackson@users.noreply.github.com>
2021-02-09 13:17:08 -0500

    Hanna Jackson Task 03

--- task03.r
+++ task03.r
@@ -29,6 +29,7 @@ ManySiblings <- replicate (1e3, length (intersect(Focus, makeBaby(Brenda, Alan))
 quantile (ManySiblings)
 mean (ManySiblings)
 plot (density(ManySiblings), main="", xlab="proportion shared genes")
+Focus will share the least DNA with the grandma, grandpa, and siblings, and more DNA with Alan and Brenda. Hence, the siblings will share more DNA with Alan and Brenda, and less DNA with Focus and the grandma and grandpa. Traits reach an equillibrium, homogenize, and then can eventually become extinct over time.
 HWE <- function (p) {
   aa <- p^2
   ab <- 2 * p * (1 - p)
@@ -47,6 +48,7 @@ Pop <- simPop(500)
 points(Pop[, "freqa"], Pop[, "Genotypes.aa"]/500, pch = 21, bg = "red")
 Pop <- simPop(50)
 points(Pop[, "freqa"], Pop[, "Genotypes.aa"]/50, pch = 22, bg = "red")
+As the genotype frequency increased, the frequency of allele aa increased. As the population size decreased, the higher the chance became that the trait will carry on.
 library (learnPopGen)
 library (learnPopGen)
 library (learnPopGen)
@@ -63,3 +65,4 @@ Line$coef
 plot(Samples, tExt)
 abline(Line)
 Line2 <- lm (tExt ~ Samples + 0)
+There is more dispersal of traits in the population. Less DNA is shared between the members of the population because of genetic drift- once you reach a finite population size there will be homogenization of a trait(s).
